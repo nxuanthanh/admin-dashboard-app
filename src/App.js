fc411c14e11d9ace8d04b618be96d3b06a257b4f
nxthanh <nxthanh@cmcglobal.vn>
2022-11-17 17:30:42 +0700

    update:  - Theme with Light and Dark Mode  - Appbar aka Topbar  - Router Setup  - Navbar aka Sidebar with React Pro Sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+import Topbar from "./scenes/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
+
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box className="app">
+          <Sidebar />
+          <Box className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </Box>
+        </Box>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import {
+  DarkModeOutlined,
+  LightModeOutlined,
+  NotificationAddOutlined,
+  PersonOutlined,
+  Search,
+  SettingsOutlined,
+} from "@mui/icons-material";
+import { Box, IconButton, InputBase, useTheme } from "@mui/material";
+import React, { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
 
 function Topbar() {
-  return <div>Topbar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box sx={{ display: "flex", justifyContent: "space-between", p: 2 }}>
+      <Box
+        sx={{
+          display: "flex",
+          backgroundColor: `${colors.primary[400]}`,
+          borderRadius: "3px",
+        }}
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <Search />
+        </IconButton>
+      </Box>
+
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlined />
+          ) : (
+            <LightModeOutlined />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationAddOutlined />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlined />
+        </IconButton>
+        <IconButton>
+          <PersonOutlined />
+        </IconButton>
+      </Box>
+    </Box>
+  );
 }
 
 export default Topbar;
